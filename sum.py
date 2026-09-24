ab7d6eaf12d905bca32a60956f8ca1c3533fc857
Dev-Moynul <stu.moynulislam@gmail.com>
2026-09-24 09:59:09 +0600

    Even odd check in list

--- sum.py
+++ sum.py
@@ -27,3 +27,21 @@ for i in range(5):
 sys.stdout.write("\n".join(r))
 
 #Multiple Test case:
+import sys
+input = sys.stdin.buffer.readline
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int,input().split()))
+    print(sum(a))
+
+#Even odd check in list:
+import sys
+input = sys.stdin.buffer.readline
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int,input().split()))
+    cnt = 0
+    for x in a:
+        if x % 2 == 0:
+            cnt += 1
+    print(cnt)
